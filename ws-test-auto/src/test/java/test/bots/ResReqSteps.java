7c3117618657b02e03b808331588dbe18292c92f
Muthukumar Ramaiyah <muthukumar_ramaiyah@cable.comcast.com>
2023-02-18 11:59:58 -0500

    added steps for schema validation

--- ws-test-auto/src/test/java/test/bots/ResReqSteps.java
+++ ws-test-auto/src/test/java/test/bots/ResReqSteps.java
@@ -3,12 +3,22 @@ package test.bots;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 
 public class ResReqSteps {
     private Response response;
+    private final RequestSpecification request = given().baseUri("https://reqres.in");
     @Given("^User exist in system$")
     public void userExistInSystem() {
         System.out.println("Lets assume user always exists in mock");
@@ -16,9 +26,7 @@ public class ResReqSteps {
 
     @When("Get User api is called with userId: {string}")
     public void getUserApiIsCalledWithUserId(String userId) {
-        System.out.println("hello: " + userId);
-        response = given()
-                .baseUri("https://reqres.in")
+        response = request
                 .get("api/users/" + userId)
                 .andReturn();
 //        //below statement to log response on console
@@ -32,6 +40,51 @@ public class ResReqSteps {
 
     @Then("Get User api response should have right schema")
     public void getUserApiResponseShouldHaveRightSchema() {
-        System.out.println("hello");
+        response.then()
+                .body(JsonSchemaValidator.matchesJsonSchema(
+                        new File("src/test/resources/schemas/getUser.json")));
+    }
+
+    @When("Get Users api is called")
+    public void getUsersApiIsCalled() {
+        response = request
+                .get("api/users")
+                .andReturn();
+    }
+
+    @Then("Get Users api should respond with response code: {int}")
+    public void getUsersApiShouldRespondWithResponseCode(int expectedCode) {
+        response.then().statusCode(expectedCode);
+    }
+
+    @Then("Get Users api response should have right schema")
+    public void getUsersApiResponseShouldHaveRightSchema() {
+        response.then()
+                .body(JsonSchemaValidator.matchesJsonSchema(
+                        new File("src/test/resources/schemas/getUsers.json")));
+    }
+
+    @When("Create User api is called with first name: {string}, last name: {string}, email: {string}, and job: {string}")
+    public void createUserApiIsCalledWithFirstLastNameAndEmail(String firstName, String lastName, String email,
+                                                               String job) throws IOException {
+        String createUserBody = Files.readString(Paths.get("src/test/resources/payloads/createUser.json"));
+        createUserBody =  createUserBody.replace("<<firstName>>", firstName)
+                        .replace("<<lastName>>", lastName)
+                        .replace("<<email>>", email)
+                        .replace("<<job>>", job);
+        System.out.println(createUserBody);
+        response = request.body(createUserBody).post().andReturn();
+    }
+
+    @Then("Create User api should respond with response code: {int}")
+    public void createUserApiShouldRespondWithResponseCode(int expectedCode) {
+        response.then().statusCode(expectedCode);
+    }
+
+    @Then("Create User api response should have right schema")
+    public void createUserApiResponseShouldHaveRightSchema() {
+        response.then()
+                .body(JsonSchemaValidator.matchesJsonSchema(
+                        new File("src/test/resources/schemas/createUser.json")));
     }
 }
--- ws-test-auto/src/test/resources/ResReq.feature
+++ ws-test-auto/src/test/resources/ResReq.feature
@@ -2,9 +2,41 @@
 Feature: ResReq Positive & Negative Testing
   Description: The purpose of this feature is test ResReq webservice
 
-  Scenario: Get User
+  @Positive @GetUsers
+  Scenario: Get Users
     Given User exist in system
-    When Get User api is called with userId: '2'
-    Then Get User api should respond with response code: 200
+    When Get Users api is called
+    Then Get Users api should respond with response code: 200
+    Then Get Users api response should have right schema
+
+  @Positive @CreateUser
+  Scenario Outline: Get User
+    When Create User api is called with first name: '<firstName>', last name: '<lastName>', email: '<email>', and job: '<job>'
+    Then Create User api should respond with response code: 201
+    Then Create User api response should have right schema
+
+    Examples:
+      | firstName | lastName   | email                        | job |
+      | test      | bots-tamil | test.bosts-tamil@hotmail.com | actor    |
+
+  @Positive @GetUser
+  Scenario Outline: Get User
+    Given User exist in system
+    When Get User api is called with userId: '<id>'
+    Then Get User api should respond with response code: <expectedCode>
     Then Get User api response should have right schema
 
+    Examples:
+      | id  | expectedCode |
+      | 2   | 200          |
+      | 1   | 200          |
+
+  @Negative @GetUser
+  Scenario Outline: Get User
+    Given User exist in system
+    When Get User api is called with userId: '<id>'
+    Then Get User api should respond with response code: <expectedCode>
+
+    Examples:
+      | id  | expectedCode |
+      | 250 | 404          |
