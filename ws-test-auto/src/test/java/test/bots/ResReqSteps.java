1f44ed1871bdf84ba8dd166f2d35a06f7f2519fe
Muthukumar Ramaiyah <muthukumar_ramaiyah@cable.comcast.com>
2023-02-19 10:04:57 -0500

    added negative and more cases

--- ws-test-auto/src/test/java/test/bots/ResReqSteps.java
+++ ws-test-auto/src/test/java/test/bots/ResReqSteps.java
@@ -3,22 +3,23 @@ package test.bots;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ResReqSteps {
     private Response response;
-    private final RequestSpecification request = given().baseUri("https://reqres.in");
+//    private final RequestSpecification request = given().baseUri("https://reqres.in");
+    private final RequestSpecification request = given().baseUri("https://reqres.in").contentType(ContentType.JSON);
     @Given("^User exist in system$")
     public void userExistInSystem() {
         System.out.println("Lets assume user always exists in mock");
@@ -72,8 +73,10 @@ public class ResReqSteps {
                         .replace("<<lastName>>", lastName)
                         .replace("<<email>>", email)
                         .replace("<<job>>", job);
-        System.out.println(createUserBody);
-        response = request.body(createUserBody).post().andReturn();
+
+//        response = request.body(createUserBody).log().all().post("/api/users").andReturn();
+        response = request.body(createUserBody).post("/api/users").andReturn();
+//        response.then().log().all();
     }
 
     @Then("Create User api should respond with response code: {int}")
@@ -86,5 +89,32 @@ public class ResReqSteps {
         response.then()
                 .body(JsonSchemaValidator.matchesJsonSchema(
                         new File("src/test/resources/schemas/createUser.json")));
+    }
+
+    @When("Delete User api is called with userId: {string}")
+    public void deleteUserApiIsCalledWithUserIdId(String userId) {
+        response = request.delete("api/users/" + userId).andReturn();
+    }
+
+    @Then("Delete User api should respond with response code: {int}")
+    public void deleteUserApiShouldRespondWithResponseCodeExpectedCode(int expectedCode) {
+        response.then().statusCode(expectedCode);
+    }
+
+    @When("Create User api is called with first name: {string}, last name: {string}, and job: {string}")
+    public void createUserApiIsCalledWithFirstNameLastNameAndJob(String firstName, String lastName,
+                                                                 String job) throws IOException {
+        String createUserBody = Files.readString(Paths.get("src/test/resources/payloads/createUserNegative.json"));
+        createUserBody =  createUserBody.replace("<<firstName>>", firstName)
+                .replace("<<lastName>>", lastName)
+                .replace("<<job>>", job);
+        response = request.body(createUserBody).post("/api/users").andReturn();
+    }
+
+    @Then("Get User api response should have correct firstName: {string}, and lastName: {string}")
+    public void getUserApiResponseShouldHaveCorrectFirstNameAndLastName(String firstName, String lastName) {
+        response.then()
+                .body("data.first_name", equalTo(firstName))
+                .body("data.last_name", equalTo(lastName));
     }
 }
--- ws-test-auto/src/test/resources/ResReq.feature
+++ ws-test-auto/src/test/resources/ResReq.feature
@@ -4,35 +4,52 @@ Feature: ResReq Positive & Negative Testing
 
   @Positive @GetUsers
   Scenario: Get Users
+  Description: The purpose of this test is check whether get users api works as expected
     Given User exist in system
     When Get Users api is called
     Then Get Users api should respond with response code: 200
     Then Get Users api response should have right schema
 
   @Positive @CreateUser
-  Scenario Outline: Get User
+  Scenario Outline: Create User
+  Description: The purpose of this test is check whether create users api works as expected
     When Create User api is called with first name: '<firstName>', last name: '<lastName>', email: '<email>', and job: '<job>'
     Then Create User api should respond with response code: 201
     Then Create User api response should have right schema
 
     Examples:
-      | firstName | lastName   | email                        | job |
-      | test      | bots-tamil | test.bosts-tamil@hotmail.com | actor    |
+      | firstName | lastName   | email                        | job   |
+      | test      | bots-tamil | test.bosts-tamil@hotmail.com | actor |
 
   @Positive @GetUser
   Scenario Outline: Get User
+  Description: The purpose of this test is check whether get user api works as expected
     Given User exist in system
     When Get User api is called with userId: '<id>'
     Then Get User api should respond with response code: <expectedCode>
     Then Get User api response should have right schema
+    Then Get User api response should have correct firstName: '<firstName>', and lastName: '<lastName>'
+
+    Examples:
+      | id | expectedCode | firstName | lastName |
+      | 1  | 200          | George    | Bluth    |
+      | 2  | 200          | Janet     | Weaver   |
+
+  @Positive @DeleteUser
+  Scenario Outline: Delete User
+  Description: The purpose of this test is check whether delete user api works as expected
+    Given User exist in system
+    When Delete User api is called with userId: '<id>'
+    Then Delete User api should respond with response code: <expectedCode>
 
     Examples:
       | id  | expectedCode |
-      | 2   | 200          |
-      | 1   | 200          |
+      | 2   | 204          |
+      | 1   | 204          |
 
-  @Negative @GetUser
+  @Negative @GetUserNegative
   Scenario Outline: Get User
+  Description: The purpose of this test is check whether get user api returns proper error code for non exist user
     Given User exist in system
     When Get User api is called with userId: '<id>'
     Then Get User api should respond with response code: <expectedCode>
@@ -40,3 +57,13 @@ Feature: ResReq Positive & Negative Testing
     Examples:
       | id  | expectedCode |
       | 250 | 404          |
+
+  @Negative @CreateUserNegative
+  Scenario Outline: Create User
+  Description: The purpose of this test is check whether create users api works as expected
+    When Create User api is called with first name: '<firstName>', last name: '<lastName>', and job: '<job>'
+    Then Create User api should respond with response code: 400
+
+    Examples:
+      | firstName | lastName   | job   |
+      | test      | bots-tamil | actor |
